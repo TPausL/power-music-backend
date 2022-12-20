3640d72b47ffdd10cb241156ca5d870be933161a
Timo Peters <timoopeters@gmail.com>
2022-12-20 19:47:42 +0100

    started backend auth
    Co-authored-by: Joshua <slaar@teckdigital.de>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-#[macro_use] extern create rocket;
+#[macro_use]
+extern crate rocket;
+extern crate dotenv;
+
+use dotenv::dotenv;
+
+mod fairings;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!";
+    "Hello, world!<!!!!"
 }
 
 #[launch]
-fn rocket -> _ {
-    rocket::build().mount("/", routes![index])
+fn rocket() -> _ {
+    dotenv().ok();
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(fairings::auth::Auth {})
 }
